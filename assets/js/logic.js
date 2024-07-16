f643f6fbf0602894b7a1ad51c1a92ef07f32c808
Nicole Alvarado Sagredo <nicolealvarado@Nicoles-MacBook-Pro.local>
2024-07-16 01:20:40 -0400

    cambios proyecto

---
+++ assets/js/logic.js
@@ -0,0 +1,31 @@
+var nav = document.querySelector('nav');
+
+window.addEventListener('scroll', function () {
+  if (window.pageYOffset > 100) {
+    nav.classList.add('bg-dark', 'shadow');
+  } else {
+    nav.classList.remove('bg-dark', 'shadow');
+  }
+});
+
+
+$(document).ready(function(){
+
+$(".card-title").click(function() {
+  $(this).next(".card-text").toggle();
+});
+
+
+$('#enviar').click(function(){
+  alert('El correo fue enviado correctamente');
+});
+
+$(document).ready(function(){
+  $('#enviar').tooltip();
+});
+
+$(document).ready(function(){
+  $('#pincha').tooltip();
+});
+
+});
